f262b71532ceaa903d608aa830157f0cb70b217c
annssva <sysoevaannaa@mail.ru>
2024-07-08 15:21:46 +0300

    added theme switcher component

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { classnames } from "shared/lib/classnames/classnames";
+import styles from './ThemeSwitcher.module.scss';
+import React from "react";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+
+
+interface ThemeSwitcherProps {
+  classname?: string;
+}
+
+export const ThemeSwitcher = ({classname}: ThemeSwitcherProps) => {
+  const {theme, toggleTheme} = useTheme();
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classnames(styles.ThemeSwitcher, {}, [classname])}
+      onClick={toggleTheme}
+    >
+
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+    </Button>
+  );
+};
