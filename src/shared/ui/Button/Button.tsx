b0b43f1ab74ea478adaa135dabdecd5f576f7a25
annssva <sysoevaannaa@mail.ru>
2024-07-18 14:28:28 +0300

    added storybook and loki

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,23 +1,19 @@
-import { classnames } from 'shared/lib/classnames/classnames';
-import { ButtonHTMLAttributes, FC } from 'react';
-import cls from './Button.module.scss';
+import { classnames } from "shared/lib/classnames/classnames";
+import { ButtonHTMLAttributes, FC } from "react";
+import cls from "./Button.module.scss";
 
 export enum ThemeButton {
-  CLEAR = 'clear',
+  CLEAR = "clear",
+  OUTLINE = "outline",
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ThemeButton;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const {
-    className,
-    children,
-    theme,
-    ...otherProps
-  } = props;
+  const { className, children, theme, ...otherProps } = props;
 
   return (
     <button
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { classnames } from 'shared/lib/classnames/classnames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import i18n from 'i18next';
-import styles from './Navbar.module.scss';
+import React from "react";
+import { classnames } from "shared/lib/classnames/classnames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import styles from "./Navbar.module.scss";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   classname?: string;
 }
 
-export const Navbar = ({ classname }: NavbarProps) => (
-  <div className={classnames(styles.Navbar, {}, [classname])}>
-    <div className={styles.links}>
-      <AppLink
-        theme={AppLinkTheme.SECONDARY}
-        to="/"
-        className={styles.mainLink}
-      >
-        {i18n.t('главная')}
-      </AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-        {i18n.t('О сайте')}
-      </AppLink>
+export const Navbar = ({ classname }: NavbarProps) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classnames(styles.Navbar, {}, [classname])}>
+      <div className={styles.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to="/"
+          className={styles.mainLink}
+        >
+          {t("Главная")}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          {t("О сайте")}
+        </AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
