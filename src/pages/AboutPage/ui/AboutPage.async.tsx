710420d8f368e237f0ebbc2079ef7d6cc5d37e91
annssva <sysoevaannaa@mail.ru>
2024-07-18 16:31:48 +0300

    fixed pipeline error

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // prettier-ignore
 export const AboutPageAsync = React.lazy(() => (
-  new Promise(resolve => {
+  new Promise((resolve) => {
     // @ts-ignore
     setTimeout(() => resolve(import('./AboutPage')), 1500);
   })
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 // prettier-ignore
 export const MainPageAsync = React.lazy(() => (
-    new Promise(resolve => {
-      // @ts-ignore
-      setTimeout(() => resolve(import('./MainPage')), 1500);
-    })
+  new Promise((resolve) => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./MainPage')), 1500);
+  })
 ));
