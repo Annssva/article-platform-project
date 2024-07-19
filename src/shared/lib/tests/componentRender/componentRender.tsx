ad13a3a525539fe5185ff21dc678d839e6e318ac
annssva <sysoevaannaa@mail.ru>
2024-07-19 16:45:48 +0300

    fixed errors

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -9,13 +9,13 @@ export interface componentRenderOptions {
 }
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
-  const { route = '/', } = options;
+  const { route = '/' } = options;
 
   return render(
-    <MemoryRouter initialEntries={ [route] }>
-      <I18nextProvider i18n={ i18nForTests }>
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nForTests}>
         { component }
       </I18nextProvider>
-    </MemoryRouter>
+    </MemoryRouter>,
   );
 }
